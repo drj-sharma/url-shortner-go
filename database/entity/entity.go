18fa15e73db7799d145752bc0eea04f05c8ccf1c
Dheeraj Sharma <53325994+drj-sharma@users.noreply.github.com>
2023-01-27 13:07:02 +0530

    add index to the short url code for faster retrieval

--- database/entity/entity.go
+++ database/entity/entity.go
@@ -3,5 +3,5 @@ package entity
 type Url struct {
 	ID           uint   `gorm:"primaryKey"`
 	OriginalUrl  string `gorm:"not null"`
-	ShortUrlCode string `gorm:"unique;not null"`
+	ShortUrlCode string `gorm:"index:idx_short_url_code,unique;not null"`
 }
